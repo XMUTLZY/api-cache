1c2069b386da494058a8f58eddcac18896d4594e
XMUTLZY <48351347+XMUTLZY@users.noreply.github.com>
2019-12-26 18:03:24 +0800

    dubbo 集成  未完成

--- src/test/java/sch/xmut/jake/cache/apicache/ApiCacheApplicationTests.java
+++ src/test/java/sch/xmut/jake/cache/apicache/ApiCacheApplicationTests.java
@@ -1,27 +1,43 @@
 package sch.xmut.jake.cache.apicache;
 
-import com.alibaba.fastjson.JSONObject;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+import sch.xmut.jake.cache.apicache.web.utils.SystemUtils;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@SpringBootTest
 class ApiCacheApplicationTests {
-    @Test
-    void contextLoads() {
-    }
-
     public static void main(String[] args) {
-        Map<Map<String, String>, String> map = new HashMap<>();
-        Map<String, String> map1 = new HashMap<>();
-        map1.put("WEB:CACHE_TEST_PROJECT_MEMBER", "TEST_BATCH1");
-        map.put(map1, "value1");
-        Map<String, String> map2 = new HashMap<>();
-        map2.put("WEB:CACHE_TEST_PROJECT_MEMBER", "TEST_BATCH2");
-        map.put(map2, "value2");
-        String json = JSONObject.toJSONString(map);
-        System.out.println(json);
+        MongoClient mongoClient = new MongoClient("106.15.207.108", 27017);//连接mongo服务器
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("redis_logs");//获取数据库
+        mongoDatabase.createCollection("test_collect");//创建集合
+        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("test_collect");//获取集合
+
+        /** 插入文档 */
+        Document document = new Document("project", "Image")
+                .append("params", "{\"key:\":123}")
+                .append("url", "/string/cache/put")
+                .append("contentType", "application/json")
+                .append("method", "POST")
+                .append("create_time", SystemUtils.dateToFormat(new Date()));
+        List<Document> documentList = Arrays.asList(document);
+        mongoCollection.insertMany(documentList);
+        System.out.println("文档插入成功");
+
+        /** 更新文档 */
+        mongoCollection.updateMany(Filters.eq("url", "/string/cache/put"), new Document("$set",new Document("url", "/string/cache/get")));
+
+        /** 查询文档 */
+        FindIterable<Document> findIterable = mongoCollection.find(); //获取迭代器
+        MongoCursor<Document> mongoCursor = findIterable.iterator(); //通过迭代器获取游标
+        while (mongoCursor.hasNext()) {
+            System.out.println(mongoCursor.next());
+        }
     }
 }
